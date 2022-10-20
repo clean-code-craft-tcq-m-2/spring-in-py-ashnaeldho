134da11048d1167cd817d8a458c40b3ae99ca38a
ashnaeldho <ashnaeldho12@gmail.com>
2022-10-20 17:46:29 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,32 @@
 
+import math as m
 def calculateStats(numbers):
-  return None
+  if type(numbers) == list:
+    length = len(numbers)
+    for i in range(length):
+      if type(numbers[i]) == str:
+        value = m.isnan(i)
+        return value
+      elif (type(numbers[i]) == float) or (type(numbers[i]) == int) and (length != 0):
+        avgVal = sum(numbers) / length
+        maxVal = max(numbers)
+        minVal = min(numbers)
+        return avgVal,minVal,maxVal
+      
+def EmailAlert(bool):
+  emailSent=False
+  return emailSent
+
+def LEDAlert(bool):
+  ledAlert = True
+  return ledAlert
+
+def StatsAlerter(maxThreshold):
+  computedStats = statistics.calculateStats([22.6, 12.5, 3.7])
+  if computedStats[2] > maxThreshold:
+    EmailAlert.emailSent = True
+    LEDAlert.ledAlert = True
+  else:
+    EmailAlert.emailSent = False
+    LEDAlert.ledAlert = False
+      
